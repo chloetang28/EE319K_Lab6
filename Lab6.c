15c227a4ddf73647a612317e7298aaf5e2a8f6b4
chloetang28 <73394953+chloetang28@users.noreply.github.com>
2021-03-18 08:54:36 -0500

    Add files via upload

---
+++ Lab6.c
@@ -0,0 +1,72 @@
+// Lab6.c
+// Runs on TM4C123
+// Use SysTick interrupts to implement a 4-key digital piano
+// MOOC lab 13 or EE319K lab6 starter
+// Program written by: put your names here
+// Date Created: 3/6/17 
+// Last Modified: 1/17/21  
+// Lab number: 6
+// Hardware connections
+// TO STUDENTS "REMOVE THIS LINE AND SPECIFY YOUR HARDWARE********
+
+
+#include <stdint.h>
+#include "../inc/tm4c123gh6pm.h"
+#include "../inc/LaunchPad.h"
+#include "../inc/CortexM.h"
+#include "Sound.h"
+#include "Key.h"
+#include "Music.h"
+#include "TExaS.h"
+
+void DAC_Init(void);         // your lab 6 solution
+void DAC_Out(uint8_t data);  // your lab 6 solution
+uint8_t Testdata;
+
+// lab video Lab6_voltmeter
+int voltmetermain(void){ //voltmetermain(void){     
+  TExaS_Init(SW_PIN_PE3210,DAC_PIN_PB3210,ScopeOn);    // bus clock at 80 MHz
+  DAC_Init(); // your lab 6 solution
+  Testdata = 15;
+  EnableInterrupts();
+  while(1){                
+    Testdata = (Testdata+1)&0x0F;
+    DAC_Out(Testdata);  // your lab 6 solution
+  }
+}
+
+// lab video Lab6_static
+int staticmain(void){   uint32_t last,now;  
+  TExaS_Init(SW_PIN_PE3210,DAC_PIN_PB3210,ScopeOn);    // bus clock at 80 MHz
+  LaunchPad_Init();
+  DAC_Init(); // your lab 6 solution
+  Testdata = 15;
+  EnableInterrupts();
+  last = LaunchPad_Input();
+  while(1){                
+    now = LaunchPad_Input();
+    if((last != now)&&now){
+       Testdata = (Testdata+1)&0x0F;
+       DAC_Out(Testdata); // your lab 6 solution
+    }
+    last = now;
+    Clock_Delay1ms(25);   // debounces switch
+  }
+}
+
+
+
+//**************Lab 6 solution below*******************
+
+int main(void){      
+  TExaS_Init(SW_PIN_PE3210,DAC_PIN_PB3210,ScopeOn);    // bus clock at 80 MHz
+  Key_Init();
+  LaunchPad_Init();
+  Sound_Init();
+  // other initialization
+  EnableInterrupts();
+  while(1){ 
+  }           
+}
+
+
