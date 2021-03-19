3366aa7df367573faabfe3ea30507576c5276bd5
chloetang28 <chloeatang@utexas.edu>
2021-03-19 10:20:02 -0500

    Update DAC.c

--- DAC.c
+++ DAC.c
@@ -2,12 +2,12 @@
 // This software configures DAC output
 // Lab 6 requires a minimum of 4 bits for the DAC, but you could have 5 or 6 bits
 // Runs on TM4C123
-// Program written by: put your names here
+// Program written by: Chloe Tang, Blake Schwartz
 // Date Created: 3/6/17 
 // Last Modified: 1/17/21 
 // Lab number: 6
 // Hardware connections
-// TO STUDENTS "REMOVE THIS LINE AND SPECIFY YOUR HARDWARE********
+#include "dac.h"   // TO STUDENTS "REMOVE THIS LINE AND SPECIFY YOUR HARDWARE********
 
 #include <stdint.h>
 #include "../inc/tm4c123gh6pm.h"
@@ -20,8 +20,19 @@
 // Output: none
 void DAC_Init(void){
 	// write this
-}
+	SYSCTL_RCGCGPIO_R |= 0x02; // Port B 
+	
+	__asm__ {
+		NOP
+		NOP
+	}
 
+	GPIO_PORTB_DIR_R |= 0x3F; // make PB5-0 outputs 
+	GPIO_PORTB_DEN_R |= 0x3F; // make PB5-0 digital 
+	
+}
+	
+	
 // **************DAC_Out*********************
 // output to DAC
 // Input: 4-bit data, 0 to 15 
@@ -29,6 +40,8 @@ void DAC_Init(void){
 // Input=n is converted to n*3.3V/15
 // or Input=n is converted to n*3.3V/63
 // Output: none
-void DAC_Out(uint32_t data){
+void DAC_Out(uint8_t data){
 	// write this
+	data = GPIO_PORTB_DATA_R; 
+	data = (data*3.3)/15; 
 }
